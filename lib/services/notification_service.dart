419bd9e00b1349b1f1b4ddf7fda0a227ff8624fa
Muhammad Raihan <muhammadraihanbjd@gmail.com>
2026-04-05 09:28:27 +0700

    feat: notification service buat project firebase

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,5 @@
+lib/services/notification_service.dart
+// lib/services/notification_service.dart
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
